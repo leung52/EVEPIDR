41776d8f558b61e032b6592b30b4bb74ae6af8b1
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-22 17:02:03 -0400

    Update lm_utils.py

--- evepidr/vep/lm_utils.py
+++ evepidr/vep/lm_utils.py
@@ -14,6 +14,20 @@ def normalise(embeddings: np.ndarray) -> np.ndarray:
     """
     return embeddings - np.mean(embeddings, axis=0)
 
+def batchify(sequences: list, batch_size: int) -> list:
+  """
+  Split sequences into batches of a specified size.
+
+  Parameters:
+  sequences (list): List of sequences.
+  batch_size (int): Size of each batch.
+
+  Returns:
+  List of batches, where each batch is a list of sequences.
+  """
+  batches = [sequences[i:i + batch_size] for i in range(0, len(sequences), batch_size)]
+  return batches
+
 def save_to_h5py(embeddings: np.ndarray, embedding_label: list, save_to_file_path: str) -> None:
     """
     """
