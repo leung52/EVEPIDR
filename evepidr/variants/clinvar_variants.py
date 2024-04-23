b8b4ff61255683f67dd080468b0925dc61df1f29
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-23 18:40:27 -0400

    Update clinvar_variants.py

--- evepidr/variants/clinvar_variants.py
+++ evepidr/variants/clinvar_variants.py
@@ -5,6 +5,8 @@ import json
 from evepidr.variants.utils import mutate_sequence
 
 def get_canonical_sequence_from_uniprot(accessions: list) -> dict:
+    """
+    """
     gene_to_sequence = {}
     
     for accession in accessions:
@@ -20,7 +22,27 @@ def get_canonical_sequence_from_uniprot(accessions: list) -> dict:
     return gene_to_sequence
 
 def get_missense_mutations_from_clinvar(gene: str) -> pd.DataFrame:
+    """
+    """
     pass
 
 def set_up_variants_dataframe(protein_seq: str, clinvar_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    """
     pass
+
+def clinvar_snp_missense_id_list(gene: str, retmax: int=10000) -> list:
+    """
+    """
+    requestURL = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=clinvar&term=(({gene}%5BGene%20Name%5D)%20AND%20%22single%20nucleotide%20variant%22%5BType%20of%20variation%5D)%20AND%20%22missense%20variant%22%5BMolecular%20consequence%5D&retmax={retmax}"
+    response = requests.get(requestURL)
+    if response.ok:
+        root = ET.fromstring(response.text)
+        ids = [id_element.text for id_element in root.findall(".//Id")]
+        if root.find(".//Count").text == '0':
+            print(f"Zero variants found for {gene}.")
+        if len(ids) < int(root.find(".//Count").text):
+            print(f'Only {len(ids)} out of {root.find(".//Count").text} variant IDs are listed. For all variant IDs, adjust retmax.')
+        return ids
+    else:
+        raise Exception: ('An error occurred', 'Request URL invalid', requestURL)
