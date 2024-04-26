1b42c8833af549866db03388794ab43b2f8ffa89
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-26 18:45:04 -0400

    Update clinvar_variants_code.py

--- report/data/clinvar_variants_code.py
+++ report/data/clinvar_variants_code.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 from evepidr.variants.clinvar_variants import *
+from evepidr.variants.add_labels_to_variants import label_if_substitution_in_idr
 
 uniprot_ids = ["O00571", "Q06787", "P17600", "P35222", "Q00839", "Q96PV0", "P15884", "O60741", "O60741", "Q02548", "Q9H165", "P15056", "Q9Y6K1", "Q9H334"]
 
@@ -15,4 +16,25 @@ clinvar_xml = clinvar_variant_info(clinvar_ids)
 clinvar_xml.write("ClinVar_data_all.xml")
 
 clinvar_df = clean_clinvar_xml_variants(genes_to_sequence, clinvar_xml)
+
+# List of IDRs sourced from MobiDB
+idrs = {
+    'BCL11A': [(17,49), (68, 173), (192, 382), (421, 741), (821, 835)],
+    'BRAF': [(1, 43), (102, 157), (274, 454), (721, 766)],
+    'CTNNB1': [(1, 222), (549, 562), (662, 781)],
+    'DDX3X': [(1, 167), (576, 662)],
+    'DNMT3A': [(1, 39), (51, 183), (198, 282), (385, 397), (431, 473), (610, 620), (832, 847)],
+    'FMR1': [(200, 213), (281, 429), (437, 632)],
+    'FOXP1': [(1, 130), (154, 179), (198, 300), (357, 447), (549, 677)],
+    'HCN1': [(1, 93), (635, 890)],
+    'HNRNPU': [(41, 281), (671, 825)],
+    'PAX5': [(1, 83), (143, 391)],
+    'SYN1': [(1, 111), (418, 705)],
+    'TCF4': [(1, 10), (28, 569), (626, 667)],
+    'PTEN': [(286, 309), (352, 403)],
+    'PTPN11': [(535, 593)],
+    'CTCF': [(1, 269), (572, 727)]
+}
+
+clinvar_xml = label_if_substitution_in_idr(clinvar_df, idrs)
 clinvar_df.to_csv("ClinVar_data_patho_benign.csv", index=False)
