a3271c19bbb1b8156d7c321b16a79e2829635c35
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-26 19:44:25 -0400

    Update add_labels_to_variants.py

--- evepidr/variants/add_labels_to_variants.py
+++ evepidr/variants/add_labels_to_variants.py
@@ -4,5 +4,14 @@ import pandas as pd
 def label_if_substitution_in_idr(variant_df: pd.DataFrame, idr_regions: dict) -> pd.DataFrame:
     """
     """
-    variant_df['Substitution Region'] = variant_df.apply(lambda row: 'IDR' if any(start <= int(row['AA Substitution'][1:-1]) <= end for start, end in idr_regions.get(row['Gene'], [])) else 'Folded', axis=1)
+    def label_region(row):
+        if pd.isna(row['AA Substitution']):
+            return 'Canon'
+        else:
+            if any(start <= int(row['AA Substitution'][1:-1]) <= end for start, end in idr_regions.get(row['Gene'], [])):
+                return 'IDR'
+            else:
+                return 'Folded'
+                
+    variant_df['Substitution Region'] = variant_df.apply(label_region, axis=1)
     return variant_df
