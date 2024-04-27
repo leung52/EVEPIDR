97bcfe9a515db61aa0da1d6515b99c520041bbfb
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-27 19:48:02 -0400

    Update pathogenicity_predictions_code.py

--- report/data/pathogenicity_predictions_code.py
+++ report/data/pathogenicity_predictions_code.py
@@ -8,10 +8,13 @@ variants_df = pd.read_csv("clinvar_data_patho_benign.csv", index=False)
 
 # ESM-1b predictions
 gene_to_sequence = fasta_to_dict("asd_linked_idps.fasta")
-sequences_for_esm1b, sequence_to_gene_and_mutation = prepare_sequences_for_lms(variants_df, protein_sequnces)
+sequences_for_esm1b, variants_df_with_sequence = prepare_sequences_for_lms(variants_df, protein_sequnces)
 model_alpha = load_esm_1b()
 embeddings_file = 'esm_1b_per_residue_embeddings.py'
 for sequences in sequences_for_esm1b:
   embed(model_alpha, sequences, embeddings_file)
 embeddings_dict = hdf5_to_dict(embeddings_file)
 reduced_embeddings = reduce(embeddings_dict.values())
+normalised_embeddings_dict = dict(zip(embeddings_dict.keys(), normalise(reduced_embeddings)))
+
+cosine_distance
