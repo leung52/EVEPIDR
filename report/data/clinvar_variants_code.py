12c2b9f72336811d1b8b8c2d0ff6cdb8d1b0fbc2
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-27 15:23:20 -0400

    Update clinvar_variants_code.py

--- report/data/clinvar_variants_code.py
+++ report/data/clinvar_variants_code.py
@@ -1,21 +1,24 @@
 import time
 import pandas as pd
+from evepidr.variants.uniprot_proteins import *
 from evepidr.variants.clinvar_variants import *
 from evepidr.variants.add_labels_to_variants import label_if_substitution_in_idr
 
 uniprot_ids = ["O00571", "Q06787", "P17600", "P35222", "Q00839", "Q96PV0", "P15884", "O60741", "O60741", "Q02548", "Q9H165", "P15056", "Q9Y6K1", "Q9H334"]
 
-genes_to_sequence = get_canonical_sequence_from_uniprot(uniprot_ids)
+gene_to_sequence, gene_to_uniprot_id = get_canonical_sequence_from_uniprot(uniprot_ids)
+
+save_sequences_as_fasta(gene_to_sequence, "asd_linked_idps.fasta")
 
 clinvar_ids = []
-for gene in genes_to_sequence.keys():
+for gene in gene_to_uniprot_id.keys():
     clinvar_ids += clinvar_snp_missense_variants_id_list(gene)
     time.sleep(0.333333334)
 
 clinvar_xml = clinvar_variant_info(clinvar_ids)
 clinvar_xml.write("clincar_data.xml")
 
-clinvar_df = clean_clinvar_xml_variants(genes_to_sequence, clinvar_xml)
+clinvar_df = clean_clinvar_xml_variants(gene_to_uniprot_id, clinvar_xml)
 
 # List of IDRs sourced from MobiDB
 idrs = {
