a402be41b3746ee732297820b7865ffe5178585f
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-28 20:56:29 -0400

    Update lm_helpers.py

--- evepidr/vep/lm_helpers.py
+++ evepidr/vep/lm_helpers.py
@@ -3,28 +3,28 @@ import numpy as np
 
 
 def batchify(sequences: list, batch_size: int) -> list:
-  """
-  Split sequences into batches of a specified size.
+    """
+    Split sequences into batches of a specified size.
 
-  Parameters:
-  sequences (list): List of sequences.
-  batch_size (int): Size of each batch.
+    Parameters:
+    sequences (list): List of sequences.
+    batch_size (int): Size of each batch.
 
-  Returns:
-  List of batches, where each batch is a list of sequences.
-  """
-  batches = [sequences[i:i + batch_size] for i in range(0, len(sequences), batch_size)]
-  return batches
+    Returns:
+    List of batches, where each batch is a list of sequences.
+    """
+    batches = [sequences[i:i + batch_size] for i in range(0, len(sequences), batch_size)]
+    return batches
 
-def save_to_h5py(embeddings: np.ndarray, sequences: list, file_path: str) -> None:
+def save_to_h5py(embeddings: np.ndarray, sequences: list, output_file: str) -> None:
     """
     """
-    with h5py.File(file_path, 'a') as hf:
-        for sequence, embedding in zip(sequences, embeddings):
-          embedding_data = np.array(embedding)
-          if sequence in hf:
-              # If the dataset already exists, overwrite the data
-              hf[sequence][...] = embedding_data
-          else:
-              # If the dataset doesn't exist, create a new one
-              hf.create_dataset(sequence, data=embedding_data, maxshape=(None,))
+    # Ensure embedding is on CPU before converting to Numpy array
+    embedding_data = embeddings.cpu().numpy()
+    
+    # Continue with saving the data to h5py file
+    with h5py.File(output_file, 'a') as h5f:
+        for seq, embed in zip(sequences, embedding_data):
+            if seq in h5f:
+                del h5f[seq]  # Delete the existing dataset
+            h5f.create_dataset(seq, data=embed, compression='gzip')
