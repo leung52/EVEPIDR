f0da7cb9a31fb6e3fea26fc7465320f0af5904df
leung52 <138917704+leung52@users.noreply.github.com>
2024-04-30 14:14:06 -0400

    Update histogram.py

--- evepidr/visualise/histogram.py
+++ evepidr/visualise/histogram.py
@@ -3,15 +3,15 @@ import numpy as np
 import pandas as pd
 
 
-def plot_predictive_scores_on_histogram(pathogenicities_df: pd.DataFrame, models_columns: ls, file_name: str):
+def plot_predictive_scores_on_histogram(pathogenicities_df: pd.DataFrame, models: list, file_name: str):
     """
     """
-    # Loop through each substitution region
+    plt.rcParams.update({'font.size': 12})
     for region in pathogenicities_df['Substitution Region'].unique():
-        fig, axes = plt.subplots(1, len(models), figsize=(15, 5), sharey=True)
+        fig, axes = plt.subplots(1, len(models), figsize=(15, 5), sharey=False)  # Set sharey to False
         fig.suptitle(f'Predictive Scores for {region} Region')
 
-        for ax, model_col in zip(axes, models_columns):
+        for ax, model_col in zip(axes, models):
             # Filter the data for the current region and model
             region_data = pathogenicities_df[pathogenicities_df['Substitution Region'] == region]
             path_scores = region_data[region_data['Pathogenicity'] == 'Pathogenic'][model_col]
@@ -19,7 +19,7 @@ def plot_predictive_scores_on_histogram(pathogenicities_df: pd.DataFrame, models
             
             # Define bins based on the combined range of pathogenic and benign scores
             combined_scores = np.concatenate((path_scores, benign_scores))
-            bins = np.linspace(np.min(combined_scores), np.max(combined_scores), 60)
+            bins = np.linspace(np.min(combined_scores), np.max(combined_scores), 50)
 
             ax.hist(path_scores, bins=bins, alpha=0.6, label='Pathogenic')
             ax.hist(benign_scores, bins=bins, alpha=0.6, label='Benign')
@@ -29,8 +29,7 @@ def plot_predictive_scores_on_histogram(pathogenicities_df: pd.DataFrame, models
             ax.set_ylabel('Count')
             ax.legend()
             ax.tick_params(axis='x', rotation=45)
-
-        plt.rcParams.update({'font.size': 12})
+            ax.autoscale(enable=True, axis='y', tight=True)  # Auto-scale the y-axis
         plt.tight_layout(rect=[0, 0, 1, 1])  # Adjust subplots to fit the figure title
-        plt.savefig(file_name)
+        plt.savefig(file_name + region + '.svg')  # Save each region to a separate file
         plt.close()
